4cc1cd84fa1d2cc1ca786d9bc311307fe803c3b5
emilianospinoso <emilianospinoso@gmail.com>
2024-01-09 08:27:00 -0300

    solutions s

---
+++ src/test/java/MaxiumAvgSubarray/SolutionSlidingWindowTest.java
@@ -0,0 +1,17 @@
+package MaxiumAvgSubarray;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionSlidingWindowTest {
+
+    @Test
+    void findMaxAverage() {
+        SolutionSlidingWindow sd = new SolutionSlidingWindow();
+        int[] nums = {1, 12, -5, -6, 50, 3};
+        int k = 4;
+        double expected=12.75000;
+        assertEquals(expected,sd.findMaxAverage(nums,k));
+    }
+}
